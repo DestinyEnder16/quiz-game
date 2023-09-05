73708d082092521dfbd54dd24ba97c1878cc1a4f
WebCoder <destinyprogrammer06@gmail.com>
2023-09-05 19:02:33 +0100

    Implemented the timer functionality.

--- src/components/FinishPage.jsx
+++ src/components/FinishPage.jsx
@@ -1,26 +1,45 @@
+import { useEffect, useState } from 'react';
+
 function FinishPage({ state, numQuestions }) {
   const percentage = (state.questionsAnswered / numQuestions) * 100;
   const margin = { marginTop: '2.5rem' };
-  return (
-    <div id="finish-text">
-      <span>
-        Final Score :{' '}
-        <strong>
-          {state.questionsAnswered} / {numQuestions}
-        </strong>
-      </span>
-      <span style={margin}>
-        (<strong>{Math.round(percentage)}</strong>%)
-      </span>
-      <button
-        onClick={() => window.location.reload()}
-        className="btn"
-        style={margin}
-      >
-        Restart Quiz
-      </button>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(function () {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <div id="finish-text">
+        <span>
+          Final Score :{' '}
+          <strong>
+            {state.questionsAnswered} / {numQuestions}
+          </strong>
+        </span>
+        <span style={margin}>
+          (<strong>{Math.round(percentage)}</strong>%)
+        </span>
+        <button
+          onClick={() => window.location.reload()}
+          className="btn"
+          style={margin}
+        >
+          Restart Quiz
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <>
+        <div className="progress-6"></div>
+        <span className="loading">Getting results...</span>
+      </>
+    );
+  }
 }
 
 export default FinishPage;
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,5 +1,33 @@
-function Timer() {
-  return <div className="timer"></div>;
+import { useEffect } from 'react';
+
+function Timer({ state, dispatch }) {
+  const mins = `${Math.floor(state.timeRemaining / 60)}`.padStart(2, '0');
+  const secs = `${state.timeRemaining % 60}`.padStart(2, '0');
+
+  const timeStyle = { fontWeight: 700, margin: '0 5px' };
+
+  useEffect(
+    function () {
+      const id = setInterval(() => {
+        if (state.timeRemaining > 0) {
+          //   setTimeRemaining((prev) => prev - 1);
+          dispatch({ type: 'tick' });
+        } else {
+          dispatch({ type: 'finish' });
+        }
+      }, 1000);
+
+      return () => clearInterval(id);
+    },
+    [state.timeRemaining, dispatch]
+  );
+
+  return (
+    <div className="timer btn" disabled={true}>
+      <span style={timeStyle}>{mins}</span>:
+      <span style={timeStyle}>{secs}</span>
+    </div>
+  );
 }
 
 export default Timer;
